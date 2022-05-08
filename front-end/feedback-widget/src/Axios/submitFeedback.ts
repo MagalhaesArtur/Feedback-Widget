c017b6649e4ca1faabed530dece30a886e81d9d7
Joao Artur <joaoartur4590@gmail.com>
2022-05-08 13:31:13 -0300

    finalizado

--- front-end/feedback-widget/src/Axios/submitFeedback.ts
+++ front-end/feedback-widget/src/Axios/submitFeedback.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://feedback-widget-project.herokuapp.com/feedback",
+  baseURL: "https://feedback-widget-project.herokuapp.com",
 });
