18942f73171a4516561139f5644804f6ec723d99
Joao Artur <joaoartur4590@gmail.com>
2022-05-08 22:01:04 -0300

    fix too large request

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,16 @@ const port = process.env.PORT;
 
 const app = express();
 app.use(cors());
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+    parameterLimit: 100000,
+  })
+);
+app.use(routes);
 
 app.use(express.json());
 app.use(routes);
